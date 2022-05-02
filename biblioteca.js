0141743b50671bfecf848808926c7e4e06cd42da
Brayan Correa <brayancorb@gmail.com>
2022-05-02 10:23:20 -0500

    ya se muestra en pantalla 1 libro agregado

--- biblioteca.js
+++ biblioteca.js
@@ -4,11 +4,16 @@ let contador=0; // variable que recorre el arreglo al agregar o quitar libros
 let inputInfoBook = document.getElementById("inputInfoBook"); //muestra el formulario para añadir libros
 let bookCard = document.getElementById("bookCard"); // muestra una tabla con la informacion introducida
 
-let inputTitleBook = document.getElementById("inputTitleBook");//obtiene el titulo del libro en el html
-let inputAuthorBook = document.getElementById("inputAuthorBook");
-let inputnumberPagesBook = document.getElementById("inputnumberPagesBook");
-let checkBox = document.getElementById("checkBox");
+let inputTitleBook = document.getElementById("inputTitleBook");//div donde se mete la info
+let inputAuthorBook = document.getElementById("inputAuthorBook");//div donde se mete la info
+let inputnumberPagesBook = document.getElementById("inputnumberPagesBook");//div donde se mete la info
 let addButtonOverlay = document.querySelector("#addButtonOverlay");
+
+let titleBook = document.getElementById("titleBook");//text area donde se muestra la informacion del titulo ingresado por usuario
+let authorBook = document.getElementById("authorBook");//text area donde se muestra la informacion del autor ingresado por usuario
+let numberPagesBook = document.getElementById("numberPagesBook");//text area donde se muestra la informacion del numero de paginas ingresado por usuario
+let checkBox = document.getElementById("checkBox");//llama al checkbox 
+let status = document.getElementById("status");
 
 addButtonOverlay.addEventListener('click',addData);
 
@@ -38,16 +43,28 @@ function on() {//crea un overlay y muestra el formulario para crear un libro
   }
 
   function addData(){//captura los datos introducidos en los input del html
+    let readStatus ="";
+
+    if(checkBox.checked == true){
+      readStatus = "yes";
+    } else {
+      readStatus = "no"
+    }
+
     let titleBook = inputTitleBook.value;
     console.log(titleBook);
     let authorBook = inputAuthorBook.value;
     console.log(authorBook);
     let pagesBook = inputnumberPagesBook.value;
     console.log(pagesBook);
-    let readStatus = "yes";
+    
+    console.log(readStatus);
+    let newBook = addBookToLibrary(titleBook,authorBook,pagesBook,readStatus);
+    console.log(newBook);
 
-    addBookToLibrary(titleBook,author,numberPages,read);
-
+    
+    return newBook;
+    
   }
 
 
@@ -71,10 +88,20 @@ function addBookToLibrary(title,author,numberPages,read) { // funcion que recibe
 
     const bookNew = new book(this.title, this.author , this.numberPages ,this.read );
 
-        return bookNew
+        titleBook.textContent = bookNew.title;
+        authorBook.textContent = bookNew.author;
+        numberPagesBook.textContent = bookNew.numberPages;
+        if(this.read === "yes"){
+          status.textContent = "read it";
+          status.style.backgroundColor = "#5FFF7A";
+        } else {
+          status.textContent = "No read It";
+          status.style.backgroundColor = "#FF4949";
+        }
+        
 
+        return bookNew;
 }   
-
 
 let bookNew = addBookToLibrary("TheHobbit","Tolkien",300,"yes");
 
@@ -87,11 +114,14 @@ bookNew.info();
 
 myLibrary[contador] = bookNew;
 
+
+
 console.log(contador);
 
 console.log(myLibrary[contador]);
 contador++;
- bookNew = addBookToLibrary("GameOfThrones","Martin",500,"No");
+
+bookNew = addBookToLibrary("GameOfThrones","Martin",500,"No");
 
  console.log(bookNew.title);
 console.log(bookNew.author);
