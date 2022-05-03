e1c7d2cbd97923e4d97309989f198d302d0ed26e
Brayan Correa <brayancorb@gmail.com>
2022-05-03 18:26:11 -0500

    ya guarda info en arreglo y muestra en pantall

--- biblioteca.js
+++ biblioteca.js
@@ -16,14 +16,16 @@ let numberPagesBook = document.getElementById("numberPagesBook");//text area don
 let checkBox = document.getElementById("checkBox");//llama al checkbox 
 let status = document.getElementById("status");
 
-addButtonOverlay.addEventListener('click',addData);
+let delete = document.getElementById
+
+addButtonOverlay.addEventListener('click',addBookToLibrary);
 
 function on() {//crea un overlay y muestra el formulario para crear un libro
     document.getElementById("overlay").style.display = "block";
     inputInfoBook.style.display = "block";
   }
 
-function createInfoCard(){
+function createInfoCard(titleBook,authorBook,pagesBook,readStatus){
 
   const newCardBook= document.createElement("newCardBook");//crea el contenedor  //da estilos a el contenedor donde se muestra la info
   newCardBook.classList.add("bookCard");
@@ -46,17 +48,27 @@ function createInfoCard(){
 
   //clase a divs para dar estilo
 
-  InputTitleBook.textContent = 'TitleBook';
+  InputTitleBook.textContent =titleBook;
   InputTitleBook.classList.add('diplayInfo');
 
-  InputAuthorBook.textContent = 'AuthorBook';
+  InputAuthorBook.textContent =authorBook;
   InputAuthorBook.classList.add('diplayInfo');
 
-  InputNumberPages.textContent = 'NumberPages';
+  InputNumberPages.textContent = pagesBook;
   InputNumberPages.classList.add('diplayInfo');
 
-  InputStatus.textContent = 'Read IT';
+  InputStatus.textContent = readStatus;
   InputStatus.classList.add('diplayInfo');
+
+  if( readStatus === "yes"){
+
+    InputStatus.style.backgroundColor = "#5FFF7A";
+    InputStatus.textContent = "read It";
+  } else {
+    
+    InputStatus.style.backgroundColor = "#FF4949";
+    InputStatus.textContent = "No read It";
+  }
 
   UpdateStatus.textContent = 'UpdateSatus';
   UpdateStatus.classList.add('diplayInfo');
@@ -64,7 +76,9 @@ function createInfoCard(){
   Delete.textContent = 'Delete';
   Delete.classList.add('delete');
 
+  newCardBook.style.display ="flex";
 
+  
 }
   
   function off() { //valida la informacion si los campos son vacios manda mensajes que la inf es requerida
@@ -74,7 +88,7 @@ function createInfoCard(){
       inputAuthorBook.placeholder = "Author requiered";
       inputnumberPagesBook.placeholder = "Insert number of Pages ";
 
-    } else if(inputAuthorBook.value === "" || inputAuthorBook.value === null && inputnumberPagesBook.value <= 0 || inputnumberPagesBook.value === null ||     inputnumberPagesBook.value === " "){
+    } else if(inputAuthorBook.value === "" || inputAuthorBook.value === null && inputnumberPagesBook.value <= 0 || inputnumberPagesBook.value === null || inputnumberPagesBook.value === " "){
 
       inputAuthorBook.placeholder = "Author requiered"
       inputnumberPagesBook.placeholder = "Insert number of Pages  ";
@@ -83,35 +97,41 @@ function createInfoCard(){
 
       inputnumberPagesBook.placeholder = "Insert number of Pages";
     } else {document.getElementById("overlay").style.display = "none";//si no faltan campos por llenar quita el display y el formulario
-            bookCard.style.display = "flex";
-            createInfoCard();
+            /*bookCard.style.display = "flex";*/
+            
     }
   }
 
-  function addData(){//captura los datos introducidos en los input del html
+  function addBookToLibrary(){//captura los datos introducidos en los input del html
     let readStatus ="";
 
     if(checkBox.checked == true){
       readStatus = "yes";
-    } else {
-      readStatus = "no"
     }
 
     let titleBook = inputTitleBook.value;
     console.log(titleBook);
+
     let authorBook = inputAuthorBook.value;
     console.log(authorBook);
+
     let pagesBook = inputnumberPagesBook.value;
     console.log(pagesBook);
     
     console.log(readStatus);
-    let newBook = addBookToLibrary(titleBook,authorBook,pagesBook,readStatus);
+
+    let newBook = new book(titleBook,authorBook,pagesBook,readStatus);
     console.log(newBook);
 
+    myLibrary.push(newBook);
+
+    console.log(myLibrary);
     
-    return newBook;
-    
+    createInfoCard(titleBook,authorBook,pagesBook,readStatus);
+
   }
+
+  console.log(myLibrary[indexArray]);
 
   function book(title,author,numberPages,read){ //objeto libro que crea libros al introducir los valores
     this.title = title;
@@ -124,29 +144,29 @@ function createInfoCard(){
     }
 }
 
-function addBookToLibrary(title,author,numberPages,read) { // funcion que recibe info del html y lo va a pasar a la funcion book
+
+/*function addBookToLibrary(title,author,numberPages,read) { // funcion que recibe info del html y lo va a pasar a la funcion book
   
     this.title = title;
     this.author = author;
     this.numberPages = numberPages;
     this.read = read;
 
-    const bookNew = new book(this.title, this.author , this.numberPages ,this.read );
+    const bookNew = new book(this.title, this.author , this.numberPages ,this.read );*/
 
-        titleBook.textContent = bookNew.title;
+       /* titleBook.textContent = bookNew.title;
         authorBook.textContent = bookNew.author;
         numberPagesBook.textContent = bookNew.numberPages;
         if(this.read === "yes"){
           status.textContent = "read it";
-          status.style.backgroundColor = "#5FFF7A";
+          status.style.backgroundColor = "#5FFF7A"; VERDE
         } else {
           status.textContent = "No read It";
-          status.style.backgroundColor = "#FF4949";
+          status.style.backgroundColor = "#FF4949";//ROJO
         }
-        
         myLibrary[indexArray] = bookNew;
         console.log(myLibrary[indexArray]);
-
+        
 }   
 
 /*let bookNew = addBookToLibrary("TheHobbit","Tolkien",300,"yes");
@@ -159,9 +179,6 @@ console.log(bookNew.read);
 bookNew.info();
 
 myLibrary[contador] = bookNew;
-
-
-
 console.log(contador);
 
 console.log(myLibrary[contador]);
